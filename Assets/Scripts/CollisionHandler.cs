1d051358e77c43a7ad2a6e71cf16e7edf5919e7f
Arswyd <vdani94@gmail.com>
2021-03-04 22:54:02 +0100

    Added Player Explosion

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other)
-    {
-        Debug.Log(this.name + " collided with" + other.gameObject.name);
-    }
+    [SerializeField] float loadDelay = 1f;
+
+    [SerializeField] ParticleSystem crashVFX;
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{this.name} triggered by {other.gameObject.name}");
+        StartCrashSequence();
+    }
+
+    void StartCrashSequence()
+    {
+        crashVFX.Play();
+        GetComponent<MeshRenderer>().enabled = false;
+        // TODO - Fix collision disabling
+        GetComponent<Collider>().enabled = false;
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadScene", loadDelay);
+    }
+
+    void ReloadScene()
+    {
+        // TODO - Fix reloadning level
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
