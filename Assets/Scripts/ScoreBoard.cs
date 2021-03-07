29616b422e6aee85db05b2ef7e7e4a5823294282
Arswyd <vdani94@gmail.com>
2021-03-07 15:55:06 +0100

    Added Scoreboard UI

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+
+    void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        DisplayScore();
+    }
 
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;
-        Debug.Log(score);
+        DisplayScore();
+    }
+
+    void DisplayScore()
+    {
+        scoreText.text = score.ToString();
     }
 }
